5414e8fabda241afbd1f8d50db6d2201a46d3c76
Michael Gharbi <mgharbi@adobe.com>
2024-01-31 22:03:22 -0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,80 @@
 import Image from "next/image";
 
+function Project({name, description, authors}) {
+  const authorList = authors.map((author, index) => {
+    // if (author == "Michaël Gharbi" ){
+    //   author = <span className="font-semibold">{author}</span>;
+    // }
+    if (index < authors.length - 1) {
+      return author + ", ";
+    } 
+    return author;
+  }).join('');
+
+  console.log(authorList);
+
+  return (
+    <div className="flex flex-row py-2">
+      <div className="w-36 h-36 relative bg-clip-border bg-violet-600"></div>
+      <div className="ps-6">
+        <h3 className="font-semibold">{name}</h3>
+        <p className="italic text-sm">{authorList}</p>
+        <p className="text-sm">{description}</p>
+      </div>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col px-24 py-12">
+      <div className="flex flex-row items-start">
+        <div className="top-0 shrink-0 w-36 h-48 relative bg-violet-600 "></div>
+        <div className="ps-6">
+          <h1 className="text-2xl">Michaël Gharbi</h1>
+          <h3 className="">Research Scientist</h3>
+          <ul className="flex flex-row">
+            <li className="pe-2">Email</li>
+            <li className="pe-2">Phone</li>
+            <li className="pe-2">Address</li>
+            <li className="pe-2">Scholar</li>
+            <li className="pe-2">Github</li>
+            <li className="pe-2">LinkedIn</li>
+            <li className="pe-2">Twitter</li>
+            <li className="pe-2">CV</li>
+            <li className="pe-2">Photography</li>
+          </ul>
+          <p className="pt-2 text-sm">
+            <span className="font-semibold">Bio: </span>
+            I am currently a Research Scientist at Adobe. I was previously a PhD
+            student at MIT CSAIL, where I had the chance to work under the
+            supervision of Prof. Frédo Durand. My research interests include
+            computational photography, computer vision and machine learning. Prior
+            to joining MIT, I completed my undergraduate studies in France, at
+            École Polytechnique, with a focus on Applied Mathematics.
+          </p>
+          <p className="pt-2 text-sm">
+            <span className="font-semibold">Internships: </span>
+            I am currently a Research Scientist at Adobe. I was previously a PhD
+            student at MIT CSAIL, where I had the chance to work under the
+            supervision of Prof. Frédo Durand. My research interests include
+            computational photography, computer vision and machine learning. Prior
+            to joining MIT, I completed my undergraduate studies in France, at
+            École Polytechnique, with a focus on Applied Mathematics.
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col items-start pt-24">
+        <Project name="One-step Diffusion with Distribution Matching Distillation" description="A cool project" authors={[
+          "Tianwei Yin", "Michaël Gharbi", "Richard Zhang", "Eli Shechtman", "Frédo Durand", "William T. Freeman", "Taesung Park"]}/>
+        <Project name="One-step Diffusion with Distribution Matching Distillation" description="A cool project" authors={[
+          "Tianwei Yin", "Michaël Gharbi", "Richard Zhang", "Eli Shechtman", "Frédo Durand", "William T. Freeman", "Taesung Park"]}/>
+        <Project name="One-step Diffusion with Distribution Matching Distillation" description="A cool project" authors={[
+          "Tianwei Yin", "Michaël Gharbi", "Richard Zhang", "Eli Shechtman", "Frédo Durand", "William T. Freeman", "Taesung Park"]}/>
+        <Project name="One-step Diffusion with Distribution Matching Distillation" description="A cool project" authors={[
+          "Tianwei Yin", "Michaël Gharbi", "Richard Zhang", "Eli Shechtman", "Frédo Durand", "William T. Freeman", "Taesung Park"]}/>
+        <Project name="One-step Diffusion with Distribution Matching Distillation" description="A cool project" authors={[
+          "Tianwei Yin", "Michaël Gharbi", "Richard Zhang", "Eli Shechtman", "Frédo Durand", "William T. Freeman", "Taesung Park"]}/>
       </div>
     </main>
   );
