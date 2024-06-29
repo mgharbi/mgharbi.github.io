f9464026f25206944c72bb6679e7b416a3c9fb8e
Michael Gharbi <michael.yanis.gharbi@gmail.com>
2024-06-28 23:54:35 -0700

    basepath

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,17 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
    */
   output: "export",
+  reactStrictMode: true,
 
   /**
    * Set base path. This is the slug of your GitHub repository.
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
+  // basePath: "/Users/mgharbi/projects/mgharbi-home/out",
+  // basePath: "/mgharbi.github.io",
   basePath: "/mgharbi.github.io",
+  // assetPrefix: "/Users/mgharbi/projects/mgharbi-home/out",
 
   /**
    * Disable server-based image optimization. Next.js does not support
