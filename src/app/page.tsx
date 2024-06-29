966d607cbc9ef4ff7088e2ca410159e60681b4d3
Michael Gharbi <michael.yanis.gharbi@gmail.com>
2024-06-28 23:00:15 -0700

    students

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -159,27 +159,142 @@ function Students() {
       "webpage": "https://tamarott.github.io", 
       "year": 2019,
     },
-    // {
-    //   "name":,
-    //   "webpage":,
-    //   "year": 2019,
-    // },
-    // {
-    //   "name":,
-    //   "webpage":,
-    //   "year": 2019,
-    // },
-    // {
-    //   "name":,
-    //   "webpage":,
-    //   "year": 2019,
-    // },
+    {
+      "name": "Zhihao Xia",
+      "webpage": "https://www.cse.wustl.edu/~zhihao.xia/",
+      "year": 2019,
+    },
+    {
+      "name": "Spandan Madan", 
+      "webpage": "https://spandan-madan.github.io/about",
+      "year": 2019,
+    },
+    {
+      "name": "Ishit Mehta",
+      "webpage": "https://scholar.google.co.in/citations?user=pUqhY-wAAAAJ&hl=en",
+      "year": 2020,
+    },
+    {
+      "name": "Mustafa Işık",
+      "webpage": "https://www.mustafaisik.net/",
+      "year": 2020,
+    },
+    {
+      "name": "Pradyumna Reddy",
+      "webpage": "https://preddy5.github.io/",
+      "year": 2020,
+    },
+    {
+      "name": "James Hong",
+      "webpage": "https://jhong93.github.io/",
+      "year": 2020,
+    },
+    {
+      "name": "Karima Ma",
+      "webpage": "https://people.csail.mit.edu/karima/home.html",
+      "year": 2020,
+    },
+    {
+      "name": "Thibaut Ehret",
+      "webpage": "https://tehret.github.io/",
+      "year": 2020,
+    },
+    {
+      "name": "Holly Jackson",
+      "webpage": "https://www.holly-jackson.com/about/",
+      "year": 2020,
+    },
+    {
+      "name": "Lucy Chai",
+      "webpage": "https://people.csail.mit.edu/lrchai/",
+      "year": 2021,
+    },
+    {
+      "name": "Sai Praveen Bangaru",
+      "webpage": "http://people.csail.mit.edu/sbangaru/",
+      "year": 2021,
+    },
+    {
+      "name": "Difan Liu",
+      "webpage": "https://difanliu.github.io/",
+      "year": 2021,
+    },
+    {
+      "name": "Camille Biscarrat",
+      "webpage": "https://www.csail.mit.edu/person/camille-biscarrat",
+      "year": 2021,
+    },
+    {
+      "name": "Jerry Hsu",
+      "webpage": "https://chichenghsu.com/",
+      "year": 2021,
+    },
+    {
+      "name": "Dave Epstein",
+      "webpage": "https://dave.ml/",
+      "year": 2021,
+    },
+    {
+      "name": "Yash Belhe",
+      "webpage": "https://yashbelhe.github.io/",
+      "year": 2022,
+    },
+    {
+      "name": "Yotam Nitzan",
+      "webpage": "https://yotamnitzan.github.io/",
+      "year": 2022,
+    },
+    {
+      "name": "Lucas Valença",
+      "webpage": "https://valenca.io/",
+      "year": 2022,
+    },
+    {
+      "name": "Caroline Chan",
+      "webpage": "https://people.csail.mit.edu/cmchan/",
+      "year": 2022,
+    },
+    {
+      "name": "Goutam Bhat",
+      "webpage": "https://goutamgmb.github.io/",
+      "year": 2022,
+    },
+    {
+      "name": "Prafull Sharma",
+      "webpage": "https://prafullsharma.net/",
+      "year": 2022,
+    },
+    {
+      "name": "Yinbo Chen",
+      "webpage": "https://yinboc.github.io/",
+      "year": 2022,
+    },
+    {
+      "name": "Ke Wang",
+      "webpage": "https://kewang0622.github.io/",
+      "year": 2022,
+    },
+    {
+      "name": "Jiteng Mu",
+      "webpage": "https://jitengmu.github.io/",
+      "year": 2023,
+    },
+    {
+      "name": "Tianwei Yin",
+      "webpage": "https://tianweiy.github.io/",
+      "year": 2023,
+    },
+    {
+      "name": "Hadi AlZayer",
+      "webpage": "https://hadizayer.github.io/",
+      "year": 2023,
+    },
   ];
   return (
     <div>
       {
         students.map((student, index) => (
-          <span key={index} className="inline after:content-[','] last:after:content-[''] pe-1">
+          <span key={index} className="inline after:content-[','] last:after:content-['.'] pe-1">
             <Link className="hover:text-sky-500" href={student.webpage}>{student.name}</Link> 
           </span>
         ))
